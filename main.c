63a4c2d4a6c8319ad392ebcd29abc1303812e87f
killolmunshi <killolmunshi@microsoft.com>
2025-12-16 11:38:59 -0500

    Testing

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[]) {
     printf("Hello, World!\n");
-    
+    int x = 8;
+    int* y = &x;
+	printf("Value of x: %d\n", x);
+	printf("pointer address of x is  %p\n", y);
+    printf("Value at addess provided by %p is %d", y, *y);
+    printf("\n%d", *y + 1);
     return 0;
 }
