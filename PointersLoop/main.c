74f3247e64972a74999f422845d8d6bc0c63048b
Killol Munshi <killolmunshi@microsoft.com>
2025-12-19 11:21:38 -0500

    more exp

--- PointersLoop/main.c
+++ PointersLoop/main.c
@@ -6,14 +6,19 @@ int main(int argc, char* argv[]) {
     int* ptr = arr;
 
     printf("\nPointer to the initial Array %p", ptr);
+    printf("\nAddress of pointer %p and its size is %zu", *(ptr), sizeof(ptr));
     printf("\nLooping through array using pointer:\n");
     for (int i = 0; i < 5; i++) {
         printf("arr[%d] = %d (address: %p)\n", i, *(ptr + i), (ptr + i));
     }
 
-    for (int j = 0; j < 5; j++) {
-        printf(" Address of each element in array %d\n", *&arr[j]);
-    }
+    int (*arrayPtr)[5] = &arr;  // Pointer to array of 5 ints
+    printf("Priting the array pointer itself %p", &arr);
+    printf("sizeof(*arrayPtr) = %zu\n", sizeof(*arrayPtr));  // 20 bytes
+
+    //for (int j = 0; j < 5; j++) {
+    //    printf(" Address of each element in array %d\n", *&arr[j]);
+    //}
 
     return 0;
 }
